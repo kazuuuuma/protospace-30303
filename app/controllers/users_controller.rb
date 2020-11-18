0b5f5ad437d84058d6ebccc7bfa70936c5ec676b
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2020-11-18 19:51:40 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def index
+  end
+  
+  def create
+    @user = User.create(user_params)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
+  end
+
+  private
+
+  def user_params
+    params.require(:users).permit(:email, :name, :profile, :occupation, :position)
+  end
+end
