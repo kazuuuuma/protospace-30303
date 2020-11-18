5473519b8632087d6ba7a6f9965c8325d733c5aa
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2020-11-18 21:47:28 +0900

    make it better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ApplicationController
+class ApplicationController < ApplicationController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
